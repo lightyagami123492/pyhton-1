fee0c31a53db74bd8bd2b317177d250fe3237d40
lightyagami123492 <baijuevon@gmail.com>
2025-07-26 07:52:47 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-num = 3
-if num > 0:
-    print(num, "is a positive number.")
+a = 10
+b = 12
+c = 0
 
-num = -1
-if num > 0:
-    print(num, "is a positive number.")
+if a and b and c:
+    print("All the numbers have boolean value as true")
+else:
+    print("At least one number have boolean value as false")
+
+a = 10
+b = -10
+c = 0
+
+if a > 0 or b > 0:
+    print("Either of the number is greater than 0")
+else:
+    print("no number is greater than 0")
