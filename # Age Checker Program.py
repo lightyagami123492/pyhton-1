1f5d837204908cbc54b3d223558f1a51a814c017
lightyagami123492 <baijuevon@gmail.com>
2025-08-01 09:29:18 -0700

    Add files via upload

---
+++ # Age Checker Program.py
@@ -0,0 +1,10 @@
+# Age Checker Program
+
+# Taking input from user
+age = int(input("Enter your age: "))
+
+# Checking if the age is between 10 and 20
+if 10 <= age <= 20:
+    print("✅ Your age is between 10 and 20.")
+else:
+    print("❌ Your age is NOT between 10 and 20.")
